eda66876a144577e83329bad6727737d65069cea
Steven Shelley <stevenshelley58@gmail.com>
2026-09-09 13:19:42 +0000

    fix: pass allowedMediaOrigins to email preview

--- src/app/suburb/[postcode]/email/page.tsx
+++ src/app/suburb/[postcode]/email/page.tsx
@@ -125,6 +125,7 @@ export default async function SuburbEmailPreviewPage({ params, searchParams }: P
     unsubscribeUrl: `${siteUrl()}/preferences/unsubscribe`,
     areaSummary: snapshotResult.summary,
     subjectStyle: style,
+    allowedMediaOrigins: configuredMediaOrigins(),
     theme,
   });
 
